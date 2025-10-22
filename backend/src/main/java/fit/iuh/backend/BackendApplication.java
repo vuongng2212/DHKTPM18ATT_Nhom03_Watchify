b6d29b64db02b2edf216dee97b4b1e01725325c1
Vuong Nguyen <kudoyaung@gmail.com>
2025-10-22 23:22:37 +0700

    feat(backend): initialize Spring Boot application

---
+++ backend/src/main/java/fit/iuh/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
